95b1b167a8d9f3abc42d6981a29d0ccdf51f1aba
Shu-Ling Fang <selina24228@gmail.com>
2019-05-01 20:46:35 +0800

    second commit

--- child.c
+++ child.c
@@ -8,12 +8,18 @@ int main(int argc,char* argv[]){
 	if(argc!=input_num){
 		err_sys("wrong input");
 	}
+	if(setCPU()<0){
+		err_sys("setCPU err");
+	}
 	wait_parrent();
+	struct timespec start=getTime();
 	printf("%s %ld\n",argv[1],getpid());
 	volatile unsigned long i;
 	for(int t=0;t<atoi(argv[2]);t++){
 		for(i=0;i<1000000UL;i++);
 	}
+	struct timespec end=getTime();
+	printk("[Project1] %ld %ld.%ld %ld.%ld\n",getpid(),start.tv_sec,start.tv_nsec,end.tv_sec,end.tv_nsec);
 	return 0;
 }
 wait_parrent(){
